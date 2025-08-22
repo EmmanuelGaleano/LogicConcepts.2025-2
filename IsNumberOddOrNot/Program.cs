4a8bdb9413b65e2ec0cea4f2a2e69c58306d7191
EmmaCodeX7 <emmacodex7@gmail.com>
2025-08-22 15:31:48 -0500

    Complete exercice 01

---
+++ IsNumberOddOrNot/Program.cs
@@ -0,0 +1,27 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.WriteLine("Ingrese numero entero o la palabra 'Salir' para salir: ");
+    numberString = Console.ReadLine();
+    if(numberString.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    { 
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt}, es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt}, es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El valor ingresado: {numberString} no es un numero entero valido");
+    }
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Saliendo del programa...");  
